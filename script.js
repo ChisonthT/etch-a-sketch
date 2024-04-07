569f0c0f079b1d3ffc7a4f6c8b574ec74a97ac83
Chisonth <1533494+ChisonthT@users.noreply.github.com>
2024-04-07 19:35:58 -0400

    Added board resizing feature.

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
-function create_gameboard(){
+function create_gameboard(size){
     let container = document.querySelector('#gameSpace');
+    let dim = Math.floor(512/size);
 
-    for (let i=0; i<256; i++){
+    
+    for (let i=0; i<(size*size); i++){
         const sqr = document.createElement('div');
         sqr.classList.add('tile');
         
+        sqr.style.height = dim +'px';
+        sqr.style.width = dim + 'px';
 
         container.appendChild(sqr);
     }
+    
 
     container.addEventListener("mouseover",(e)=> {
         if (e.target.classList.contains('tile')) {
-            console.log("it happened");
+            
             e.target.style.backgroundColor = 'red';
         }
     })
 }
 
+function resizeGameBoard(){
 
-create_gameboard();
+    let size = prompt("Please enter an integer between 1-100 for the \
+    new size of the board", "");
+
+    size = Number(size);
+    let container = document.querySelector('#gameSpace');
+
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+    
+    create_gameboard(size);
+}
+
+create_gameboard(16);
+
+let btn = document.querySelector('button');
+
+btn.addEventListener("click", resizeGameBoard);
+
+
+
